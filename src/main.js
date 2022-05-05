6c13b14282069954f412609ad2b75acf03b646d4
rafaelpereira <rafael.pereira.s@zoho.com>
2022-05-05 10:31:54 -0300

    isWeekDay implemented

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+function isWeekDay(day){
+    // It is faster but very input dependant. 
+    // If the input is ever not of type "<date?>(<3_digit_weekday>)" it shouldn't work.
+    // As it wasn't warned that the date type could be different from the examples and tests, I'll assume it doesn't change.
+    return day[day.length-4] != 's'
+}
+
 function getCheapestHotel (input) { //DO NOT change the function's name.
     return "Cheapest hotel name"
 }
 
 exports.getCheapestHotel = getCheapestHotel
+exports.isWeekDay = isWeekDay
