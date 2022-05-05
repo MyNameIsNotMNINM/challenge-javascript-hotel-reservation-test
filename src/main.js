d27abfc5c16d86240a78efe6aed8dca99f9d3a90
rafaelpereira <rafael.pereira.s@zoho.com>
2022-05-05 10:42:05 -0300

    tdd countWeekends test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+function getFirstLetterOfWeekDay(day){
+    return day[day.length-4];
+}
+
 function isWeekDay(day){
     // It is faster but very input dependant. 
     // If the input is ever not of type "<date?>(<3_digit_weekday>)" it shouldn't work.
     // As it wasn't warned that the date type could be different from the examples and tests, I'll assume it doesn't change.
-    return day[day.length-4] != 's'
+    return getFirstLetterOfWeekDay(day) != 's'
 }
+
+
 
 function getCheapestHotel (input) { //DO NOT change the function's name.
     return "Cheapest hotel name"
--- test/main-test-spec.js
+++ test/main-test-spec.js
@@ -12,4 +12,11 @@ describe('test', function () {
     it('isWeekDay_monday_true', function () {
         expect(isWeekDay("16Mar2009(mon)")).to.equal(true);
     });
+
+    it('countWeekends_0_0', function () {
+        expect(countWeekends(["16Mar2009(mon)", "17Mar2009(tue)", "18Mar2009(wed)", "19Mar2009(thu)", "20Mar2009(fri)"])).to.equal(true);
+    });
+    it('countWeekends_5_5', function () {
+        expect(countWeekends(["15Mar2009(sun)", "01May2022(sun)", "25Jun2022(sat)", "17June2023(sat)", "05Nov2023(sun)"])).to.equal(true);
+    });
 })
